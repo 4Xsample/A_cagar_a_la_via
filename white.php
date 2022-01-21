b9fb079de068d2554a421bd36a81f9c8a8bc7212
4Xsample <4Xsample@gmail.com>
2022-01-22 00:34:50 +0100

    A cagar

---
+++ white.php
@@ -0,0 +1,53 @@
+<?php
+  $array = [
+    "L’Opus Dei", 
+    "L’adoctrina-ment de TV3", 
+    "La variant Òmicron", 
+    "Un penis superheroi que lluita per la justícia",
+    "Tsunami Democràtic",
+    "El confinament",
+    "La Batalla de l'Ebre",
+    "Gabriel Rufian",
+    "Esquerra",
+    "Castellers",
+    "El temps",
+    "La santa espina",
+    "Puta Espanya", 
+    "“Los catalanes hacen cosas”", 
+    "Un pixapins anant a caçar bolets armat", 
+    "Plats bruts",
+    "Pere Aragones",
+    "Carles Puigdemont",
+    "Miquel iceta",
+    "En Shin-Chan fent el monstre del cul",
+    "Conills voladors assassins",
+    "Camps de concentració",
+    "Una unitat antidisturbis fent un examen de mates",
+    "Anar de vacances a Susqueda i no tornar",
+    "Un borbó a l'atzar", 
+    "Un penis de 35 cm", 
+    "L’accent de Girona", 
+    "Francisco Franco",
+    "Les cançons de Bola de Drac en castellà",
+    "El burkini",
+    "Adolf Hitler",
+    "Abascal",
+    "Sopa de cabra",
+    "Sangtrait",
+    "Tallar les vies del AVE a Girona",
+    "El jutje Marchena",
+    "Els volcans",
+    "Amputats",
+    "El meu compte bancari",
+    "Porno alenany sadomasoquista",
+    "adriannumeritos"
+    
+];
+  $min = 0;
+  $max = count($array)-1;
+
+  $rand = rand($min,$max);
+
+  echo $array[$rand]
+
+?>
