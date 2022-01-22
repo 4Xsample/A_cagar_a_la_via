5808cb4aed7c7d4be3d6f6516d5d099b6f9289f1
flaytos <davidsarriqsflay@gmail.com>
2022-01-22 20:45:18 +0100

    Update black.php
    Afegides mes opcions

--- black.php
+++ black.php
@@ -9,8 +9,27 @@
     "Què acabarà portant Catalunya a la independència?",
     "Quin és el secret d’una vida llarga i saludable?",
     "Què vaig trobar amagat al lavabo del regional?",
-    "La nova moda, ser negacionista de:"
-    "La nova incorporació a Creadors es:"
+    "La nova moda, ser negacionista de:",
+    "La nova incorporació a Creadors es:",
+    "Durant l'enterrament de l'àvia, no podia deixar de pensar en:",
+    "Quin es el pitjor malson d'un alcalde de la CUP?",
+    "Jo no sóc racista, simplement m'agrada més",
+    "Quin és el meu vici secret?",
+    "A que dedicaré el meu pròxim voluntariat?",
+    "Què és el que més troba a faltar d'Àfrica una persona despreés de 15 anys a Europa?",
+    "Què va causar el divorci dels avis?",
+    "Què va causar el naixement de VOX?",
+    "Què li encanta al president segons els mitjans de comunicació?",
+    "Què dir en ple sexe per posar calenta a la teva parella",
+    "Què ès el millor per animar una cita a cegues?",
+    "Què va causar l'atac de cor de l'avi?",
+    "Què feia tanta pudor dins la nevera?",
+    "Quan tiro canyes, m'asseguro que a l'altra persona li agradi".
+    "Què va trobar la meva exparella a l'armari?"
+    "Què fa que una festa sigui una festa?",
+    "No em van ascendir a la feina quan van descobrir que m'encanta",
+    "Què és el primer en el que penso després de fer un bon polvo?",
+    "Què tenim en comú els que estem veient aquest streaming?"
 ];
   $min = 0;
   $max = count($array)-1;
