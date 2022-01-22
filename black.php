44000be53710d75a3f10cf76ce7345883d65c84f
4Xsample <4Xsample@gmail.com>
2022-01-22 13:14:01 +0100

    Update black.php

--- black.php
+++ black.php
@@ -5,8 +5,6 @@
     "Quina serà la nova i trencadora activitat a la pròxima Festa dels Súpers?", 
     "Què passa pel cap d’un votant de VOX quan introdueix la papereta a l’urna?",
     "Quin es el meu vici secret?",
-    "Què m’agrada fer tan bon punt arribo a una platja nudista?",
-    "No hi ha res millor que una bona dutxa després de ____.",
     "Què provocarà la 3a Guerra Mundial?",
     "Què acabarà portant Catalunya a la independència?",
     "Quin és el secret d’una vida llarga i saludable?",
