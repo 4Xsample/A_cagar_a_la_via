8707a9febf4f12797556667387ebf2c2c7c470e3
Jordi Rossell <4Xsample@gmail.com>
2023-01-22 13:39:20 +0100

    .

--- black.php
+++ black.php
@@ -1,36 +1,34 @@
 <?php
 
-  $array = [
-
-    "Quin és el regal més català que et pot cagar el tió?", 
-    "Quin era el hobby més secret de Francisco Franco, que només feia en nits de lluna plena?", 
-    "Quina serà la nova i trencadora activitat a la pròxima Festa dels Súpers?", 
-    "Què passa pel cap d’un votant de VOX quan introdueix la papereta a l’urna?",
-    "Quin es el meu vici secret?",
-    "Què provocarà la 3a Guerra Mundial?",
-    "Què acabarà portant Catalunya a la independència?",
-    "Quin és el secret d’una vida llarga i saludable?",
-    "Què vaig trobar amagat al lavabo del regional?",
-    "La nova moda, ser negacionista de:",
+$array = [
+    'Quin és el regal més català que et pot cagar el tió?',
+    'Quin era el hobby més secret de Francisco Franco, que només feia en nits de lluna plena?',
+    'Quina serà la nova i trencadora activitat a la pròxima Festa dels Súpers?',
+    'Què passa pel cap d’un votant de VOX quan introdueix la papereta a l’urna?',
+    'Quin es el meu vici secret?',
+    'Què provocarà la 3a Guerra Mundial?',
+    'Què acabarà portant Catalunya a la independència?',
+    'Quin és el secret d’una vida llarga i saludable?',
+    'Què vaig trobar amagat al lavabo del regional?',
+    'La nova moda, ser negacionista de:',
     "Durant l'enterrament de l'àvia, no podia deixar de pensar en:",
     "Quin es el pitjor malson d'un alcalde de la CUP?",
     "Jo no sóc racista, simplement m'agrada més",
-    "Quin és el meu vici secret?",
-    "Què va causar el divorci dels avis?",
-    "Què va causar el naixement de VOX?",
+    'Quin és el meu vici secret?',
+    'Què va causar el divorci dels avis?',
+    'Què va causar el naixement de VOX?',
     "Què va causar l'atac de cor de l'avi?",
-    "Què feia tanta pudor dins la nevera?",
+    'Què feia tanta pudor dins la nevera?',
     "Quan tiro canyes, m'asseguro que a l'altra persona li agradi",
     "Què va trobar la meva exparella a l'armari?",
-    "Què fa que una festa sigui una festa?",
+    'Què fa que una festa sigui una festa?',
     "No em van ascendir a la feina quan van descobrir que m'encanta",
-    "Què és el primer en el que penso després de fer un bon polvo?"
+    'Què és el primer en el que penso després de fer un bon polvo?',
+];
 
-  ];
-
-    $min = 0;
-    $max = count($array)-1;
-    $rand = rand($min,$max);
-    echo $array[$rand]
+$min = 0;
+$max = count($array) - 1;
+$rand = rand($min, $max);
+echo $array[$rand];
 
 ?>
