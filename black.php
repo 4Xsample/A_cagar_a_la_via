b0fec0fb4729994833aeefdfe4f3303f24ed08e2
Jordi Rossell <4Xsample@gmail.com>
2023-02-05 03:09:52 +0100

    Update black.php

--- black.php
+++ black.php
@@ -1,8 +1,8 @@
 <?php
 
+// Aquí tenim una llista de frases absurdes
 $array = [
     'Quin és el regal més català que et pot cagar el tió?',
-    'Quin era el hobby més secret de Francisco Franco, que només feia en nits de lluna plena?',
     'Quina serà la nova i trencadora activitat a la pròxima Festa dels Súpers?',
     'Què passa pel cap d’un votant de VOX quan introdueix la papereta a l’urna?',
     'Quin es el meu vici secret?',
@@ -10,25 +10,32 @@ $array = [
     'Què acabarà portant Catalunya a la independència?',
     'Quin és el secret d’una vida llarga i saludable?',
     'Què vaig trobar amagat al lavabo del regional?',
-    'La nova moda, ser negacionista de:',
-    "Durant l'enterrament de l'àvia, no podia deixar de pensar en:",
-    "Quin es el pitjor malson d'un alcalde de la CUP?",
-    "Jo no sóc racista, simplement m'agrada més",
+    'Durant l’enterrament de l’àvia, no podia deixar de pensar en:',
+    "Jo no sóc racista, simplement m'agrada més:",
     'Quin és el meu vici secret?',
     'Què va causar el divorci dels avis?',
-    'Què va causar el naixement de VOX?',
+    'Què va causar el naixement de vox?',
     "Què va causar l'atac de cor de l'avi?",
     'Què feia tanta pudor dins la nevera?',
     "Quan tiro canyes, m'asseguro que a l'altra persona li agradi",
     "Què va trobar la meva exparella a l'armari?",
     'Què fa que una festa sigui una festa?',
     "No em van ascendir a la feina quan van descobrir que m'encanta",
-    'Què és el primer en el que penso després de fer un bon polvo?',
+    'Què és el primer en què penso després de fer un bon clau?',
+    "El secret a veus que ningú vol reconèixer és que no som independents per culpa de",
+    "L'ingredient secret de l'escudella de l'àvia era",
+    "Des que he deixat de mirar TV3 trobo a faltar",
+    "La pròxima cançó d'Els Catarres va sobre",
+    "L'herencia de l'avi al final era"
 ];
 
+// Aquí calculen un número aleatori, perquè? Doncs perquè...
 $min = 0;
 $max = count($array) - 1;
 $rand = rand($min, $max);
+
+// I aquí simplement imprimeixen la frase aleatòria
 echo $array[$rand];
 
+// I això és tot... quin codi més inútil
 ?>
